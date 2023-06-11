f558bd16be046fb6af74362729cb8a5e64b03c78
nuttdguy <pygnasakdev@gmail.com>
2023-06-10 19:33:03 -0500

    Add a data table placeholder

--- yield/src/app/app.module.ts
+++ yield/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { AnalyticComponent } from './analytic/analytic.component';
 import { PanelLeftComponent } from './analytic/panel-left/panel-left.component';
 import { PanelRightComponent } from './analytic/panel-right/panel-right.component';
+import { DataTableComponent } from './data-table/data-table.component';
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent, AnalyticComponent, PanelLeftComponent, PanelRightComponent],
+  declarations: [AppComponent, TopBarComponent, AnalyticComponent, PanelLeftComponent, PanelRightComponent, DataTableComponent],
   imports: [BrowserModule, AppRoutingModule, NgChartsModule],
   providers: [],
   bootstrap: [AppComponent],
