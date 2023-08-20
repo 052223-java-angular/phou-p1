c898d2c4fd5967d3a896c9cf48306b0175eb8808
nuttdguy <pygnasakdev@gmail.com>
2023-08-20 18:57:52 -0500

    add not found component

--- yield/src/app/app-routing.module.ts
+++ yield/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MarketsComponent } from './views/markets/markets.component';
 import { PortfolioComponent } from './views/portfolio/portfolio.component';
 import { TradesComponent } from './views/trades/trades.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'markets', pathMatch: 'full'},
@@ -10,25 +11,22 @@ const routes: Routes = [
     children: [
       {path: 'view', component: MarketsComponent},
       {path: 'research', component: MarketsComponent},
-      {path: 'detail', component: MarketsComponent},
-      {path: '**', component: MarketsComponent}
+      {path: 'detail', component: MarketsComponent}
   ]},
   {path: 'trades', component: TradesComponent, 
     children: [
       {path: '', redirectTo: 'view', pathMatch: 'full'},
       {path: 'view', component: TradesComponent},
       {path: 'analysis', component: TradesComponent},
-      {path: 'records', component: TradesComponent},
-      {path: '**', component: TradesComponent}
+      {path: 'records', component: TradesComponent}
   ]},
   {path: 'portfolio', component: PortfolioComponent, 
     children: [
       {path: 'view', component: PortfolioComponent},
       {path: 'analysis', component: PortfolioComponent},
-      {path: 'entries', component: PortfolioComponent},
-      {path: '**',component: PortfolioComponent}
+      {path: 'entries', component: PortfolioComponent}
     ]},
-  {path: '**', component: MarketsComponent}
+  {path: '**', component: NotfoundComponent}
 ];
 
 @NgModule({
